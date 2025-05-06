3fdc85f335fbbe08133e3e8c28e2666a6e705504
edWard0-i <116932458+edWard0-i@users.noreply.github.com>
2025-05-06 17:42:18 -0400

    Primer Commit

---
+++ monitoreo/src/test/java/com/edutech/monitoreo/MonitoreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.monitoreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
