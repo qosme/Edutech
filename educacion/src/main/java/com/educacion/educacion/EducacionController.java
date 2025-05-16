78c77ffb9f9f9fb88545c890307837f7bab757bf
leonardgonz <leonardogonzalezriquelme@gmail.com>
2025-05-15 21:45:27 -0400

    Prueba de conexion

---
+++ educacion/src/main/java/com/educacion/educacion/EducacionController.java
@@ -0,0 +1,21 @@
+package com.educacion.educacion;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EducacionController {
+
+    @GetMapping("/saludo")
+    public String saludo() {
+        return "¡Bienvenido a la aplicación de Educación!";
+    }
+}
+
+
+
+
+
+
+
+
