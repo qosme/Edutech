ad8b3faf664eb3e8fb75707506238fd179c8f4ca
dani-gonzalezf <dani.gonzalezf@duocuc.cl>
2025-05-17 09:47:35 -0400

    se agrega microservicio administracion

--- educacion/src/main/java/com/educacion/educacion/EducacionController.java
+++ educacion/src/main/java/com/educacion/educacion/EducacionController.java
@@ -9,6 +9,9 @@ public class EducacionController {
     @GetMapping("/saludo")
     public String saludo() {
         return "¡Bienvenido a la aplicación de Educación!";
+
+
+                
     }
 }
 
